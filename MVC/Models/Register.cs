623b44060ecb2f828916f5cb389a5dcce1a45977
suryasunder96 <33921806+suryasunder96@users.noreply.github.com>
2019-02-28 15:49:59 +0530

    Add files via upload

---
+++ MVC/Models/Register.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mvc.Models
+{
+    public class Register
+    {
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Name")]
+        public string Rname { get; set; }
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Address")]
+        [DataType(DataType.MultilineText)]
+        public string Raddress { get; set; }
+        public Course courselist { get; set; }
+        public string Gender { get; set; }
+        
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+
+        
+        public string Remail { get; set; }
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Username")]
+        public string Rusername { get; set; }
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Rpassword { get; set; }
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string Rconfirm { get; set; }
+    }
+
+    public enum Course
+    {
+        BCA,
+        MCA,
+        BCOM,
+        MBA
+    }
+
+}
---
+++ MVC/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mvc.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage="Cant be empty")]
+        [Display(Name="Student Name")]
+       // [RegularExpression()]
+        public string Sname { get; set; }
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Student Address")]
+        [DataType(DataType.MultilineText)]
+        public string Saddress { get; set; }
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Student Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Semail { get; set; }
+        public Gender genderlist { get; set; }
+    }
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
