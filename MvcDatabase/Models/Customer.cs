545c21c7c1e14d3a5601cb91de689f2a0f589aef
suryasunder96 <33921806+suryasunder96@users.noreply.github.com>
2019-03-01 16:36:18 +0530

    Add files via upload

---
+++ MvcDatabase/Models/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcDatabase.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Name")]
+        public string Cname { get; set; }
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Address")]
+        [DataType(DataType.MultilineText)]
+        public string Caddress { get; set; }
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Cemail { get; set; }
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Username")]
+        public string Cusername { get; set; }
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Cpassword { get; set; }
+    }
+}
---
+++ MvcDatabase/Models/CustomerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcDatabase.Models
+{
+    public class CustomerContext:DbContext
+    {
+        public CustomerContext()
+            : base("mycon")
+        {
+            DropCreateDatabaseIfModelChanges<CustomerContext> d = new DropCreateDatabaseIfModelChanges<CustomerContext>();
+            Database.SetInitializer<CustomerContext>(d);
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
